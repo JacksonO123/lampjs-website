c1bdd1b03b1e4f0b0fe17fc2b26413a59b031beb
Jackson Otto <jacksonotto120@gmail.com>
2023-12-14 22:30:56 -0700

    test serverless function

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-import vercelHelper from '@jacksonotto/lampjs-vercel';
+// import vercelHelper from '@jacksonotto/lampjs-vercel';
 
-export default vercelHelper;
+// export default vercelHelper;
+
+import { readdirSync } from 'fs';
+
+export default async function handler(_request, response) {
+  const dir = readdirSync('.');
+  response.status(200).json({ dir });
+}
